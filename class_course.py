1e16ee3ef430001e164ac2d5c85bc9b592a229c5
Slabestek <baskuiken@gmail.com>
2017-11-01 15:26:35 +0100

    class_vak heet nu class_course en class_student is aangemaakt

--- class_course.py
+++ class_course.py
@@ -1,4 +1,4 @@
-class vak:
+class course:
 	def __init__(self, name, hcol = 0, wcol = 0, prac = 0):
 		self.name = name
 		self.hcol = hcol
---
+++ class_student.py
@@ -0,0 +1,10 @@
+class student:
+	def __init__(self, lname, fname, studnr, course1, course2, course3, course4, course5):
+		self.lname = lname
+		self.fname = fname
+		self.studnr = studnr
+		self.course1 = course1
+		self.course2 = course2
+		self.course3 = course3
+		self.course4 = course4
+		self.course5 = course5
