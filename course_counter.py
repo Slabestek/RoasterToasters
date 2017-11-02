01f27888a3764d264cb4c5c5e8571592d8e75db5
Slabestek <baskuiken@gmail.com>
2017-11-02 14:45:13 +0100

    toevoegen van course_counter

---
+++ course_counter.py
@@ -0,0 +1,14 @@
+import csv
+import collections
+
+course1 = collections.Counter()
+with open('studentenenvakken.csv') as input_file:
+	for row in csv.reader(input_file, delimiter = ','):
+		course1[row[3]] += 1
+		course1[row[4]] += 1
+		course1[row[5]] += 1
+		course1[row[6]] += 1
+		course1[row[7]] += 1
+
+print "Number of students in course: %s" % course1['Calculus 2']
+print course1.most_common()
