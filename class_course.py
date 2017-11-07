35fa4366f08bce8477948b028434688dbabaab2e
Slabestek <baskuiken@gmail.com>
2017-11-07 14:16:45 +0100

    ptoevoegen updates classes

--- class_course.py
+++ class_course.py
@@ -1,12 +1,12 @@
 class course:
-	def __init__(self, name, hcol = 0, wcol = 0, prac = 0, studnrs = []):
+	def __init__(self, name, hcol = 0, wcol = 0, prac = 0, studnrs = {}):
 		self.name = name
 		self.hcol = hcol
 		self.wcol = wcol
 		self.prac = prac
 		self.studnrs = studnrs
 
-	def studnrs(self, studnr):
+	def enroll(self, studnr):
 		self.studnrs.append(studnr)
 
 
@@ -15,7 +15,7 @@ heuristieken = course("heuristieken", 2, 1, 0)
 for i in range(10):
 	heuristieken.studnrs = i
 
-print heuristieken.studnrs
+print heuristieken
 
 
 
--- class_student.py
+++ class_student.py
@@ -1,10 +1,6 @@
 class student:
-	def __init__(self, lname, fname, studnr, course1, course2, course3, course4, course5):
+	def __init__(self, lname, fname, studnr, course = []):
 		self.lname = lname
 		self.fname = fname
 		self.studnr = studnr
-		self.course1 = course1
-		self.course2 = course2
-		self.course3 = course3
-		self.course4 = course4
-		self.course5 = course5
+		self.course = course
