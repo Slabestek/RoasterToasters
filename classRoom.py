93611353fde318d50974015b15aa82fc5fcf09ac
BoudewijnB <boudewijnbauw@gmail.com>
2017-11-07 15:24:54 +0100

    added csv reader in classRoom

--- classRoom.py
+++ classRoom.py
@@ -1,4 +1,13 @@
+import csv
+
 class room:
 	def __init__(self, name, cap):
 		self.name = name
 		self.cap = cap
+	
+	my_list = []
+	
+	with open('rooms&cap.csv', 'r') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			my_list.append(row[0], row[1])
