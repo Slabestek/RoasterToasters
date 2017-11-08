70359e5e938773075ae549d213e839d03d8ff4a7
Slabestek <baskuiken@gmail.com>
2017-11-08 13:34:38 +0100

    classCourse.py heeft nu een functie, courseReader, die csv bestanden inleest met courses

--- classCourse.py
+++ classCourse.py
@@ -1,5 +1,8 @@
+import csv
+
 class course:
-	def __init__(self, name, lecture = 0, seminar = 0, seminarCap = None, practical = 0, practicalCap = None, studentNumbers = {}):
+	def __init__(self, name, lecture = 0, seminar = 0, seminarCap = None, practical = 0, 
+				practicalCap = None, studentNumbers = {}):
 		self.name = name
 		self.lecture = lecture
 		self.seminar = seminar
@@ -8,6 +11,9 @@ class course:
 		self.practicalCap = practicalCap
 		self.studentNumbers = studentNumbers
 
+	def __repr__(self):
+		return self.name
+
 	def enroll(self, studentNumber):
 		self.studentNumbers.append(studentNumber)
 
@@ -15,8 +21,24 @@ class course:
 heuristieken = course("heuristieken")
 
 
-print heuristieken
-print len(heuristieken.studentNumbers)
+def courseReader(file):
+
+	myList = []
+
+	# read in csv of courses
+	with open(file, 'r') as f:
+		next(f)
+		reader = csv.reader(f)
+		for row in reader:
+			myList.append(row)
+
+	# make a dictionary with a key-value pair for each course
+	courseDict = {row[0]: course(row[0], row[1], row[2], row[3], row[4], row[5], row[6]) 
+		for row in myList}
+
+	return courseDict
+
+
 
 
 
