84af4416748629cd6dd0d37291dd5392a4d1ba8d
Esmee <esmee_hoen@hotmail.com>
2017-11-14 13:23:28 +0100

    aanpassing score funtie

--- score.py
+++ score.py
@@ -1,33 +1,40 @@
-from ? import ?
+#from ? import ?
 
 
-score = 0
+def doubleStudent(scheduleList):
+	for day in days:
+		for timeslot in timeslots:
+			for classroom in classrooms:
+				print scheduleList[0][1][0].studentNumbers
 
-if rooster = 128
-	score = 1000
-	for (elke activiteit (i in range 128?)):
 
-		if student dubbel
-			score -1
 
-		if student te veel
-			score -1
 
-	elke activiteit vergelijken op verdeling week
+# score = 0
 
-		if verdeeld
-			score + 20
+# if rooster = 128
+# 	score = 1000
+# 	for (elke activiteit (i in range 128?)):
 
-			
+# 		if student dubbel
+# 			score -1
 
-		if x - 1 
-			score - 10
+# 		if student te veel
+# 			score -1
 
-		if x - 2
-			score - 20
+# 	elke activiteit vergelijken op verdeling week
 
-		if x - 3
-			score - 30		
+# 		if verdeeld
+# 			score + 20
 
-return score
+# 		if x - 1 
+# 			score - 10
+
+# 		if x - 2
+# 			score - 20
+
+# 		if x - 3
+# 			score - 30		
+
+# return score
 
