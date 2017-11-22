41d2b08254f390f5eaafb8c7bd184318369d99ca
Slabestek <baskuiken@gmail.com>
2017-11-22 14:47:26 +0100

    changed variable name

--- classCourse.py
+++ classCourse.py
@@ -2,7 +2,7 @@ import csv
 
 class Course:
 	def __init__(self, name, lecture = 0, seminar = 0, seminarCap = 0, practical = 0, 
-				practicalCap = 0, students = 0, studentNumbers = {}, activities = [],  seminars = 0, 
+				practicalCap = 0, studentAmount = 0, studentNumbers = {}, activities = [],  seminars = 0, 
 				practicals = 0):
 		self.name = name
 		self.lecture = lecture
@@ -17,29 +17,29 @@ class Course:
 		except:
 			pass	
 		self.studentNumbers = studentNumbers
-		self.students = int(students)
+		self.studentAmount = int(studentAmount)
 		
 		if self.seminar > 0:
-			if self.students > self.seminarCap:
-				remainder = self.students % self.seminarCap
+			if self.studentAmount > self.seminarCap:
+				remainder = self.studentAmount % self.seminarCap
 				if remainder == 0:
-					seminars = (self.students / self.seminarCap) * self.seminar
+					seminars = (self.studentAmount / self.seminarCap) * self.seminar
 				else:
-					seminars = (((self.students - remainder) / self.seminarCap) + 1) * self.seminar
+					seminars = (((self.studentAmount - remainder) / self.seminarCap) + 1) * self.seminar
 		if self.practical > 0:			
-			if self.students > self.practicalCap:
-				remainder = self.students % self.practicalCap
+			if self.studentAmount > self.practicalCap:
+				remainder = self.studentAmount % self.practicalCap
 				if remainder == 0:
-					practicals = (self.students / self.practicalCap) * self.practical
+					practicals = (self.studentAmount / self.practicalCap) * self.practical
 				else:
-					practicals = (((self.students - remainder) / self.practicalCap) + 1) * self.practical
+					practicals = (((self.studentAmount - remainder) / self.practicalCap) + 1) * self.practical
 		self.seminars = seminars
 		self.practicals = practicals
 
 	def __repr__(self):
 		return self.name
 
-	def enrollCourse(classObject):
+	def enrollCourse(self, classObject):
 		self.studentNumbers[classObject.studentNumber] = classObject
 
 
