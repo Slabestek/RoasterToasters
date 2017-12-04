9bf9b52cedbc0d4f5ef77e094abb220e53ced28d
Esmee <esmee_hoen@hotmail.com>
2017-12-04 11:01:23 +0100

    doubleStudent in score kind of working

--- modules/score.py
+++ modules/score.py
@@ -1,53 +1,65 @@
-import scheduleRange
-
-score = 0
-
-def ValidSchedule(scheduleList):
-
-	# for activity in activities:
-	# 	if activity:
-	# 		score += 1000
+# import scheduleRange
 
 
-	# for day in days:
-	# 	for timeslot in timeslots:
-	# 		for classroom in classrooms:
+# score = 0
+
+# def ValidSchedule(scheduleList):
+
+# 	# for activity in activities:
+# 	# 	if activity:
+# 	# 		score += 1000
+
+
+# 	# for day in days:
+# 	# 	for timeslot in timeslots:
+# 	# 		for classroom in classrooms:
 
 
 
 def doubleStudent(scheduleList):
-	#for day in days:
-		#for timeslot in timeslots:
-			#for classroom in classrooms:
-				for student in scheduleList[day][timeslot][classroom].studentNumbers:			
-					for classroom in classrooms:
-						for students in scheduleList[day][timeslot][classroom].studentNumbers:
-							if student == students :
-								score -= 1
-	return score
 
-def ExtraStudent(scheduleList):
+	score = 0
+	for day in range (5):
+		for timeslot in range (4):
+			for classroom in range (7):
+				if scheduleList[day][timeslot][classroom] is None:
+					pass
+				else:
+					for student in scheduleList[day][timeslot][classroom].studentNumbers:		
+						for classroom in range(7):
+							if scheduleList[day][timeslot][classroom] is None:
+								pass
+							else:
+								for students in scheduleList[day][timeslot][classroom].studentNumbers:
+									if student == students:
+										score -= 1	
+	print(score)				
+											# return score	
+											
+										
 
-	for student in scheduleList [day][timeslot][classroom].studentNumbers:
-		for classroom in classrooms:
-			if student > seminarCap or student > practicalCap:
-				score -= 1
-	return score		
+# def ExtraStudent(scheduleList):
 
-def ScheduleSpread1(scheduleList):
+	# for student in scheduleList [day][timeslot][classroom].studentNumbers:
+	# 	for classroom in classrooms:
+	# 		if student > seminarCap or student > practicalCap:
+	# 			score -= 1
+	# print(score)		
 
-		score -= 10
-	return score
+# def ScheduleSpread1(scheduleList):
 
-def ScheduleSpread2(scheduleList):
+# 		score -= 10
+# 	return score
 
-		score -= 20
-	return score	
+# def ScheduleSpread2(scheduleList):
 
-def ScheduleSpread3(scheduleList):
+# 		score -= 20
+# 	return score	
 
-		score -= 30
-	return score	
+# def ScheduleSpread3(scheduleList):
+
+# 		score -= 30
+# 	return score	
 
 
 
