ae0667660f18ef638ae75abfa3ad2e01e307eb11
Esmee <esmee_hoen@hotmail.com>
2017-12-06 12:45:16 +0100

    extraStudent

---
+++ algorithms/hillClimber.py
@@ -0,0 +1,36 @@
+# generate random rooster
+# swap function (days, activities)
+from random import randint as randint
+from modules.helpers import swapRoomSlot as swapRoomSlot
+from modules.helpers import randomSchedule as randomSchedule
+from modules.scheduleRange import days, timeslots, rooms
+from modules.init import createObjects as createObjects
+
+objectList = createObjects()
+roomObj = objectList[2]
+print(roomObj)
+
+def climbHill(scheduleList, activities, iterations):
+
+    # generate a random schedule
+    randomSchedule(scheduleList, days, timeslots, rooms, activities, roomObj)
+    object1 = None
+    object2 = None
+    for i in range(iterations):
+        while object1 == None:
+            rDay = randint(0, days - 1)
+            rTime = randint(0, timeslots - 1)
+            rRoom = randint(0, rooms - 1)
+            object1 = scheduleList[rDay][rTime][rRoom]
+        print(object1)
+        while object2 == None:
+            rDay1 = randint(0, days - 1)
+            rTime1 = randint(0, timeslots - 1)
+            rRoom1 = randint(0, rooms - 1)
+            object2 = scheduleList[rDay1][rTime1][rRoom1]
+        print(object2)
+        print(scheduleList[object1.day][object1.timeslot])
+        print(scheduleList[object2.day][object2.timeslot])
+        swapRoomSlot(object1, object2, scheduleList)
+        print(scheduleList[object1.day][object1.timeslot])
+        print(scheduleList[object2.day][object2.timeslot])
