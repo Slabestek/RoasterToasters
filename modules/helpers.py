1d1be2553409ec10126672042997fd47cf4f6f96
Slabestek <baskuiken@gmail.com>
2017-12-06 16:08:27 +0100

    fixed bug with hillClimber

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,4 +1,4 @@
-from random import shuffle as shuffle
+import random
 
 def swapRoomSlot(activity1, activity2, scheduleList):
 
@@ -9,17 +9,19 @@ def swapRoomSlot(activity1, activity2, scheduleList):
     room1 = activity1.roomIndex
     room2 = activity2.roomIndex
 
-    activity1.roomChange(activity2, room2, day2, timeslot2)
-    activity2.roomChange(activity1, room1, day1, timeslot1)
+    activity1.roomChange(activity2.room, room2, day2, timeslot2)
+    activity2.roomChange(activity1.room, room1, day1, timeslot1)
     temp = scheduleList[day2][timeslot2][room2]
     scheduleList[day2][timeslot2][room2] = scheduleList[day1][timeslot1][room1]
     scheduleList[day1][timeslot1][room1] = temp
 
 def randomSchedule(scheduleList, days, timeslots, rooms, activities, roomObj):
     counter = 0
+
+    # random.shuffle(activities)
     l = len(activities)
-    print(l)
-    shuffle(activities)
+    length = 0
+    nonecount=0
     for day in range(days):
         for timeslot in range(timeslots):
             for room in range(rooms):
@@ -28,7 +30,14 @@ def randomSchedule(scheduleList, days, timeslots, rooms, activities, roomObj):
                 if counter >= l:
                     break
                 if scheduleList[day][timeslot][room] == None:
-                    break
+                    # print(scheduleList[day][timeslot][room])
+                    nonecount+=1
+                    # break
                 else:
+                    # print(scheduleList[day][timeslot][room])
                     scheduleList[day][timeslot][room].roomChange(
                         roomObj[room], room, day, timeslot)
+            length += len(scheduleList[day][timeslot])
+
+    # print(nonecount, counter)
+    # print(l, length)
