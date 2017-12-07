8588e2e3ad5f9b6f22a477882eeb1101bd74c0c1
Esmee <esmee_hoen@hotmail.com>
2017-12-07 10:23:11 +0100

    update score.py

--- modules/score.py
+++ modules/score.py
@@ -1,20 +1,16 @@
-# import scheduleRange
-# from classes.classRoom import Room
-# from modules.init import createObjects
-
-# score = 0
-
-# def ValidSchedule(scheduleList):
-
-# 	# for activity in activities:
-# 	# 	if activity:
-# 	# 		score += 1000
-
-
-# 	# for day in days:
-# 	# 	for timeslot in timeslots:
-# 	# 		for classroom in classrooms:
-
+def validSchedule(scheduleList):
+	score = 0
+	array = []
+	countAct = 0
+	for day in range(5):
+		for timeslot in range(4):
+			for classroom in range(7):
+				activity = scheduleList[day][timeslot][classroom]
+				array.append(activity)
+				if len(array) == 129:
+					score = 1000	
+	print(score)
+	print(countAct)				
 
 
 def doubleStudent(scheduleList):
@@ -30,63 +26,45 @@ def doubleStudent(scheduleList):
 							if scheduleList[day][timeslot][classroom]:
 								for students in scheduleList[day][timeslot][classroom].studentNumbers:
 									if student == students:
-										score -= 1
-							else:
-								pass		
-				else:
-					pass					
-										
+										score -= 1																			
 	print(score)				
 								
 
 def extraStudent(scheduleList):
 	score = 0
-	array = []
 	for day in range(5):
 		for timeslot in range(4):
 			for classroom in range(7):
 				if scheduleList[day][timeslot][classroom]:	
 					studentLength = len(scheduleList[day][timeslot][classroom].studentNumbers)
-					array.append(scheduleList[day][timeslot][classroom].room)
-					print(len(array))
 					roomCap = int(scheduleList[day][timeslot][classroom].room.cap)	
 					if studentLength > roomCap:
-						score -= studentLength - roomCap
-				else:
-					pass		
-
+						score -= studentLength - roomCap	
 	print(score)		
 
 
 def scheduleSpread1(scheduleList):
 
 	score = 0
+	countAct = 0
 	for day in range(5):
 		for timeslot in range(4):
 			for classroom in range(7):
-				for activity in activities:
-					score -= 10
-			
+				activity = scheduleList[day][timeslot][classroom]
+				if activity:
+					for day in range(5):
+						activityCheck = scheduleList[day][timeslot][classroom]
+						if activityCheck:
+							if activity == activityCheck:
+								countAct += 1
+								# if countAct 
 
 
 
-
-# for i in courses:
-# 	counter =+ 1
-# 	for j in counter[i]:
-
-	# score = 0
-	# countCourse = 0
-	# countAct = 0
-
-	# for day in range (5):
+					# score -= 10
+					
 		
 
-
-
-
-# 		score -= 10
-# 	return score
 
 # def ScheduleSpread2(scheduleList):
 
